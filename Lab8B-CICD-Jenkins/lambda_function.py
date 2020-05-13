e98913b38c172fc1f4041fa553dd08ff57f06cf5
aotemang <gongting19970227@163.com>
2020-05-13 13:00:51 +0800

    修改文件

--- Lab8B-CICD-Jenkins/lambda_function.py
+++ Lab8B-CICD-Jenkins/lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello Gongxiaoting1111112222!')
+        'body': json.dumps('Hello Gongxiaoting1111112222222!')
     }
